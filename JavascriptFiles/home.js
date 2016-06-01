e3f84c4c0d229bc60a0452159b180bfb48652421
blue945u <blue945u@gmail.com>
2016-06-01 14:02:47 +0200

    [homepage] fixed background music play and pause

--- JavascriptFiles/home.js
+++ JavascriptFiles/home.js
@@ -9,3 +9,15 @@ $(function(){
     document.getElementById("usernameLogin").innerHTML = name;
     document.getElementById("usericon").src = pic;
 });
+function playPause() { //play and pause backgroundmusic
+    var music = document.getElementById('music2'); 
+    var music_btn = document.getElementById('music_btn2'); 
+    if (music.paused){ 
+        music.play(); 
+        music_btn.src = 'pic/On.png'; 
+    } 
+    else{ 
+        music.pause(); 
+        music_btn.src = 'pic/Off.png';  
+    } 
+} 
